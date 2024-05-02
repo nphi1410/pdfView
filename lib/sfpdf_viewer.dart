808f332cc31f9da0c20c943a7b966ebf6315607e
DevPhiCap <phi.np.1953@aptechlearning.edu.vn>
2024-05-02 15:40:37 +0700

    update icon effect

--- lib/sfpdf_viewer.dart
+++ lib/sfpdf_viewer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:pdfrx/pdfrx.dart';
 
@@ -11,9 +12,9 @@ class MyPdfViewer extends StatefulWidget {
 }
 
 class _MyPdfViewerState extends State<MyPdfViewer> {
-  String x = '0', y = '0', pageNum='0';
-  double? pageWidth;
-  double? pageHeight;
+  String x = '0', y = '0', pageNum = '0';
+  double? pageWidth, widDx, pageHeight, widDy, totalHeight;
+  double? zoomLevel=1.0;
 
   @override
   void initState() {
@@ -26,42 +27,74 @@ class _MyPdfViewerState extends State<MyPdfViewer> {
       PdfDocument document = await PdfDocument.openUri(Uri.parse(widget.url));
       if (document != null && document.pages.isNotEmpty) {
         setState(() {
-          pageWidth = document.pages[0].width;
-          pageHeight = document.pages[0].height;
+          pageWidth = document.pages[0].width.toDouble();
+          pageHeight = document.pages[0].height.toDouble();
+          totalHeight = document.pages.length.toDouble() * (pageHeight ?? 0);
         });
       } else {
-        // Handle the case when the document is null or has no pages
         print('Error: Unable to load PDF document');
       }
     } catch (e) {
-      // Handle any exceptions that occur during document loading
       print('Error: $e');
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double aspectRatio = screenWidth / (pageWidth ?? 1);
+    double fitWidth = (pageWidth ?? 0) * aspectRatio;
+    double fitHeight = (totalHeight ?? 0) * aspectRatio;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.blue,
-        title: Text('PDF Viewer'),
+        title: Text('height:${pageHeight?.toStringAsFixed(2)}/ ${totalHeight?.toStringAsFixed(2)}'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
-            child: SfPdfViewer.network(
-              widget.url,
-              pageSpacing: 5.0,
-              onTap: (pagePosition) {
-                setState(() {
-                  x = pagePosition.pagePosition.dx.toStringAsFixed(4);
-                  y = (pageHeight! - pagePosition.pagePosition.dy).toStringAsFixed(4);
-                  pageNum = pagePosition.pageNumber.toString();
-                });
-              },
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Container(
+                  width: fitWidth,
+                  height: fitHeight,
+                  child: Stack(
+                    children: [
+                      SfPdfViewer.network(
+                        widget.url,
+                        pageSpacing: 5.0,
+                        onTap: (details) {
+                          setState(() {
+                            widDx = details.position.dx;
+                            widDy = details.position.dy;
+                            x = details.pagePosition.dx.toStringAsFixed(2);
+                            y = (pageHeight! - details.pagePosition.dy).toStringAsFixed(2);
+                            pageNum = details.pageNumber.toString();
+                          });
+                        },
+                        onZoomLevelChanged: (details) {
+                          setState(() {
+                            zoomLevel = details.newZoomLevel;
+                          });
+                          print('widX:$widDx');
+                          print('widY:$widDy');
+                          },
+
+                      ),
+                      if (widDx != null && widDy != null && x != '-1.00' && y != '-1.00')
+                        Positioned(
+                          top: widDy! - 18,
+                          left: widDx! - 6,
+                          child: Icon(Icons.edit),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
           Container(
